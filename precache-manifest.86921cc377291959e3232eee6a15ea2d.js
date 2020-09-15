e6f784f374a27638c99c13fb70b5f7709d0bcc92
Gonzalo Galvez <ggalvezb@live.com>
2020-09-15 13:51:24 -0300

    Updates

---
+++ precache-manifest.86921cc377291959e3232eee6a15ea2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed96e198dadc6181290464fc7cb56c7",
+    "url": "/dashboard_cuentas/index.html"
+  },
+  {
+    "revision": "9bc4ae6f01612de88a93",
+    "url": "/dashboard_cuentas/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "e54663e2a154c39060e8",
+    "url": "/dashboard_cuentas/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9bc4ae6f01612de88a93",
+    "url": "/dashboard_cuentas/static/js/2.f3311aa0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dashboard_cuentas/static/js/2.f3311aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54663e2a154c39060e8",
+    "url": "/dashboard_cuentas/static/js/main.0791c203.chunk.js"
+  },
+  {
+    "revision": "cd3c808094546da69093",
+    "url": "/dashboard_cuentas/static/js/runtime-main.6fa48fb0.js"
+  }
+]);
